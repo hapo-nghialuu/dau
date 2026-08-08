9ce1b06decc53c715a66e5aa6af9bf2cb026a7df
hapo-nghialuu <nghialt@haposoft.com>
2026-08-08 16:48:37 +0700

    chore(release): update cask for v0.1.7

--- Casks/dau.rb
+++ Casks/dau.rb
@@ -1,6 +1,6 @@
 cask "dau" do
   version "0.1.7"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "c33c6eb5db634f8ae8d2179c92c812613df475b3813d4037fc6634215b9dfd8f"
 
   url "https://github.com/hapo-nghialuu/dau/releases/download/v#{version}/Dau-#{version}.zip"
   name "Dấu"
